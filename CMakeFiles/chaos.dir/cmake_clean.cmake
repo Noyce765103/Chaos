c3c4eba8e89164b1ae53cbe4a72ecaf26c5c8149
Noyce's Tencent cloud1 <noyce.liujiaqi@outlook.com>
2021-01-24 16:48:46 +0800

    modify log system

---
+++ CMakeFiles/chaos.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chaos.dir/chaos/log.cc.o"
+  "lib/libchaos.pdb"
+  "lib/libchaos.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chaos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
